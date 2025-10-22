6e250d544d5720a825c23d4fa7b09d3733ca79de
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-10-22 17:37:29 +0530

    initlize react app with vite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import appRoutes from "./Routes";
-import { RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={appRoutes} />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
