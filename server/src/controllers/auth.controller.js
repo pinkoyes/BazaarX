da688b59004642c572b850c0707034d5c4d5377b
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-11-07 18:10:41 +0530

    add google auth

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -6,15 +6,75 @@ import {
   registerSchema,
   loginSchema,
 } from "../utils/validations/auth.validation.js";
+import { OAuth2Client } from "google-auth-library";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const isProduction = process.env.NODE_ENV === "production";
 
 const cookieOptions = {
   httpOnly: true,
   secure: isProduction,
-  sameSite: isProduction ? "noen" : "lax",
+  sameSite: isProduction ? "none" : "lax",
   maxAge: 1 * 24 * 60 * 60 * 1000,
 };
+
+export const googleAuth = asyncHandler(async (req, res) => {
+  const { googleAuthToken } = req.body;
+
+  if (!googleAuthToken) {
+    throw new ApiError(400, "Token is missing");
+  }
+
+  const ticket = await client.verifyIdToken({
+    idToken: googleAuthToken,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  const payload = ticket.getPayload();
+
+  const { name, email, picture, sub: googleId } = payload;
+
+  if (!email) {
+    throw new ApiError(400, "Google account has no email");
+  }
+
+  let user = await User.findOne({ $or: [{ email }, { googleId }] });
+
+  let isNewUser = false;
+
+  if (!user) {
+    user = await User.create({
+      fullName: name,
+      email,
+      googleId,
+      authProvider: "google",
+      profileImage: picture,
+    });
+    isNewUser = true;
+  }
+  const token = user.generateToken();
+
+  return res
+    .status(200)
+    .cookie("token", token, cookieOptions)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: {
+            id: user._id,
+            name: user.fullName,
+            email: user.email,
+            role: user.role,
+            profileImage: user.profileImage,
+            authProvider: user.authProvider,
+          },
+        },
+        isNewUser ? "User created successfully" : "Login successfully!"
+      )
+    );
+});
 
 export const RegisterUser = asyncHandler(async (req, res) => {
   const parseData = registerSchema.safeParse(req.body);
@@ -53,8 +113,8 @@ export const RegisterUser = asyncHandler(async (req, res) => {
           user: {
             id: newUser._id,
             fullName: newUser.fullName,
-            email: newUser.email,
-            phoneNumber: newUser.phoneNumber,
+            email: newUser?.email,
+            phoneNumber: newUser?.phoneNumber,
             role: newUser.role,
             authProvider: newUser.authProvider,
             profileImage: newUser.profileImage,
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -4,12 +4,14 @@ import {
   LoginUser,
   LogoutUser,
   CurrentUser,
+  googleAuth,
 } from "../controllers/auth.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", RegisterUser);
+router.post("/google", googleAuth);
 router.post("/login", LoginUser);
 router.post("/logout", protect, LogoutUser);
 router.get("/me", protect, CurrentUser);
