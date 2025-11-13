c05d4fd13ad41de9f0a4df5cbe85f35d192fed9a
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-11-13 23:32:38 +0530

    remove prod code

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: [process.env.CLIENT_ORIGIN, "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
