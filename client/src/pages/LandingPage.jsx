35e8c76c4b5c4b28bd466c5eaaec1cefa5c69b24
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-11-17 11:11:51 +0530

    remove extra authentication check from landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,7 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import { useAuth } from "../context/AuthContext";
 
 const LandingPage = () => {
-  const { isAuthenticated } = useAuth();
-  const navigate = useNavigate();
-
-  if (isAuthenticated) navigate("/home");
-
   return (
     <div className="relative w-full min-h-[calc(100vh-7rem)] bg-linear-to-br from-indigo-600 via-purple-600 to-pink-500 flex items-center justify-center text-white overflow-hidden">
       {/* ===== Background Blobs ===== */}
